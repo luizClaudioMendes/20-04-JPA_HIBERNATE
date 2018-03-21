f85973efb364dd84a2f6022cbf6b81e0d5e6b049
luiz claudio <luiz_claudio_cassio_mendes@hotmail.com>
2018-03-20 22:19:52 -0300

    6.6. Fazendo Join entre entidades -- usando JPQL

---
+++ 07-consultasLocaldoraVeiculoJPQL/src/com/algaworks/curso/jpa2/consultas/ConsultaAcessoropPorModeloCarro.java
@@ -0,0 +1,32 @@
+package com.algaworks.curso.jpa2.consultas;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.util.JPAUtil;
+
+public class ConsultaAcessoropPorModeloCarro {
+	
+	public static void main(String[] args) {
+		EntityManager em = JPAUtil.createEntitymanager();
+		
+		StringBuilder sb = new StringBuilder(0);
+		sb.append(" SELECT a.descricao ");
+		sb.append(" FROM Carro c");
+		sb.append(" JOIN c.acessorios a"); //linha do join
+		sb.append(" WHERE c.modelo.descricao = 'Cruze'");
+		
+		
+		List<String> resultado = em.createQuery(sb.toString(), String.class).getResultList();
+		
+		for (String acessorio : resultado) {
+			System.out.println(acessorio);
+		}
+		
+		em.close();
+		
+	}
+
+}
