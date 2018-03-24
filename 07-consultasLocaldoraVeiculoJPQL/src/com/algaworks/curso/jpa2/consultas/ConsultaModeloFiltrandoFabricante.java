4a689f9e82e8ec3e81948ea540f773830e86f139
luiz claudio <luiz_claudio_cassio_mendes@hotmail.com>
2018-03-24 10:08:51 -0300

    6.9. Passando parâmetros para queries -- usando concatenacao, posicao e por variavel

--- 07-consultasLocaldoraVeiculoJPQL/src/com/algaworks/curso/jpa2/consultas/ConsultaModeloFiltrandoFabricante.java
+++ 07-consultasLocaldoraVeiculoJPQL/src/com/algaworks/curso/jpa2/consultas/ConsultaModeloFiltrandoFabricante.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import com.algaworks.curso.jpa2.modelo.Fabricante;
 import com.algaworks.curso.jpa2.util.JPAUtil;
 
 public class ConsultaModeloFiltrandoFabricante {
@@ -12,9 +11,36 @@ public class ConsultaModeloFiltrandoFabricante {
 	public static void main(String[] args) {
 		EntityManager em = JPAUtil.createEntitymanager();
 		
-		List<String> modelos = em.createQuery("select mc.descricao from ModeloCarro mc where mc.fabricante.nome = 'ford'", String.class).getResultList();
+		String fabricante = "ford";
 		
-		for (String modelo : modelos) {
+		//MENOS ELEGANTE
+		List<String> modelos1 = em.createQuery("select mc.descricao from ModeloCarro mc where mc.fabricante.nome = '"+ fabricante +"'", String.class).getResultList();
+		
+		for (String modelo : modelos1) {
+			System.out.println(modelo);
+		}
+		
+		System.out.println("------------------------------------------------------------------------------");
+		
+		/*
+		 *  Positional parameter are considered deprecated; use named parameters or JPA-style positional parameters instead
+		 *  
+		 *  devido ao erro acima, comentamos a parte do codigo abaixo
+		 */
+		
+//		List<String> modelos2 = em.createQuery("select mc.descricao from ModeloCarro mc where mc.fabricante.nome = ? ", String.class).setParameter(1, fabricante).getResultList();
+//		
+//		for (String modelo : modelos2) {
+//			System.out.println(modelo);
+//		}
+		
+		
+		System.out.println("------------------------------------------------------------------------------");
+		
+		//MAIS ELEGANTE
+		List<String> modelos3 = em.createQuery("select mc.descricao from ModeloCarro mc where mc.fabricante.nome = :fabricante ", String.class).setParameter("fabricante", fabricante).getResultList();
+		
+		for (String modelo : modelos3) {
 			System.out.println(modelo);
 		}
 		
---
+++ 07-consultasLocaldoraVeiculoJPQL/src/com/algaworks/curso/jpa2/consultas/ConsultaPassandoParametros.java
@@ -0,0 +1,25 @@
+package com.algaworks.curso.jpa2.consultas;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.util.JPAUtil;
+
+public class ConsultaPassandoParametros {
+	
+	public static void main(String[] args) {
+		EntityManager em = JPAUtil.createEntitymanager();
+		
+		List<String> modelos = em.createQuery("select mc.descricao from ModeloCarro mc where mc.fabricante.nome = 'ford'", String.class).getResultList();
+		
+		for (String modelo : modelos) {
+			System.out.println(modelo);
+		}
+		
+		em.close();
+		
+	}
+
+}
